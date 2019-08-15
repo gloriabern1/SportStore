ef36781444a6cea848b479fe4611a5c1a7b61faf
goddess12 <gloonyi12@gmail.com>
2019-08-15 12:54:25 +0100

    first commit

---
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SportsStore.Models.ViewModels;
+
+namespace SportsStore.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes="page-model")]
+    public class PageLinkTagHelper : TagHelper
+    {
+        private IUrlHelperFactory urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory helperFactory)
+        {
+            urlHelperFactory = helperFactory;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+
+        public ViewContext ViewContext { get; set; }
+
+        public PagingInfo PageModel { get; set; }
+        public string PageAction { get; set; }
+
+        //properties for styling
+        public bool pageClassesEnabled { get; set; } = false;
+        public string pageClass { get; set; }
+        public string pageClassNormal { get; set; }
+        public string pageClassSelected { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            TagBuilder result = new TagBuilder("div");
+            for(int i=1; i<=PageModel.TotalPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.Attributes["href"] = urlHelper.Action(PageAction, new { productPage = i });
+                // also for styling
+                if (pageClassesEnabled)
+                {
+                    tag.AddCssClass(pageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage
+                        ? pageClassSelected : pageClassNormal);
+                }
+                tag.InnerHtml.Append(i.ToString());
+                result.InnerHtml.AppendHtml(tag);
+            }
+            output.Content.AppendHtml(result.InnerHtml);
+        }
+    }
+}
---
+++ SportsStore/Models/SeedData.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            ApplicationDbContext context = app.ApplicationServices.GetRequiredService<ApplicationDbContext>();
+            context.Database.Migrate();
+            if (!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product
+                    {
+                        Name = "Kayak",
+                        Description = "A boat for one person",
+                        Category = "Watersports",
+                        Price = 275
+                    },
+                    new Product
+                    {
+                        Name = "LifeJacket",
+                        Description = "Protective and fashionable",
+                        Category = "Watersports",
+                        Price = 48.84m
+                    },
+                    new Product
+                    {
+                        Name = "soccer ball",
+                        Description = "Fifa approved size and weight",
+                        Category = "Soccer",
+                        Price = 34.5m
+                    },
+                    new Product
+                    {
+                        Name = "Corner flags",
+                        Description = "filed professional touch",
+                        Category = "Soccer",
+                        Price = 56.3m
+                    },
+                    new Product
+                    {
+                        Name = "Stadium",
+                        Description = "flat packed with people",
+                        Category = "Soccer",
+                        Price = 79500
+                    },
+                     new Product
+                     {
+                         Name = "Thinking cap",
+                         Description = "improve brain efficiency by 35%",
+                         Category = "Chess",
+                         Price = 27
+                     },
+                      new Product
+                      {
+                          Name = "Usteady Chair",
+                          Description = "give oppotent a disadvantag",
+                          Category = "Chess",
+                          Price = 27.5m
+                      }
+
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
